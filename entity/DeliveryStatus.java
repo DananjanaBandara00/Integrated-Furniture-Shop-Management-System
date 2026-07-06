f90afb466cba67819322f2b891a2e190732b56e0
Dananjana_Bandara <sandaruwanibandara30@gmail.com>
2026-07-06 17:51:28 +0530

    Delete entity directory

--- entity/DeliveryStatus.java
+++
@@ -1,5 +0,0 @@
-package com.webs.furniturewebs.entity;
-
-public enum DeliveryStatus {
-    PENDING, SHIPPED, IN_TRANSIT, DELIVERED, CANCELLED
-}
--- entity/PaymentStatus.java
+++
@@ -1,8 +0,0 @@
-package com.webs.furniturewebs.entity;
-
-public enum PaymentStatus {
-    PENDING,
-    SUCCESS,
-    FAILED,
-    REFUNDED
-}
