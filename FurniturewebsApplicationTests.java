48e8f68adef83cb64d0c4d8c73bac18d30bfdcc8
Dananjana_Bandara <sandaruwanibandara30@gmail.com>
2026-07-06 18:07:49 +0530

    Add files via upload

---
+++ FurniturewebsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webs.furniturewebs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurniturewebsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
