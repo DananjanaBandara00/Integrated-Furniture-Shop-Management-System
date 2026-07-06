22d1accb4083b6a7df713e40af0c6c2a7d777190
Dananjana_Bandara <sandaruwanibandara30@gmail.com>
2026-07-06 17:56:54 +0530

    Delete src/main/java/config/repository directory

--- src/main/java/config/repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package com.webs.furniturewebs.repository;
-import com.webs.furniturewebs.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AddressRepository extends JpaRepository<Address, Integer> {
-    List<Address> findByUser_UserId(Integer userId);
-}
--- src/main/java/config/repository/OfferRepository.java
+++
@@ -1,9 +0,0 @@
-package com.webs.furniturewebs.repository;
-
-import com.webs.furniturewebs.entity.Offer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface OfferRepository extends JpaRepository<Offer, Integer> {
-    List<Offer> findByIsActiveTrue();
-}
