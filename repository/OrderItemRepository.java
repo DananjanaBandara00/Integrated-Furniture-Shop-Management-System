67062fea1995013b87b78f7a95b2bf3e428ea670
Dananjana_Bandara <sandaruwanibandara30@gmail.com>
2026-07-06 17:51:19 +0530

    Delete repository directory

--- repository/OrderItemRepository.java
+++
@@ -1,5 +0,0 @@
-package com.webs.furniturewebs.repository;
-import com.webs.furniturewebs.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {}
